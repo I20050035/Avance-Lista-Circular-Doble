98a23ffa514f10bcab9823de2f679dcf33df203b
I20050035 <70992098+I20050035@users.noreply.github.com>
2021-10-16 19:40:32 -0500

    Add files via upload

---
+++ Lista Doble Circular/Lista Doble Circular/Lista.cs
@@ -0,0 +1,264 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Lista_Doble_Circular
+{
+    class Lista
+    {
+        // profe si nota alugunos errores es porque estamos haciendo las correspondientes adaptaciones y hemos tenido problemas para hacer que funcionen :C
+        //lista Circular doble ... head y cual es el final
+        private Nodo head;
+
+        public Nodo Head
+        {
+            get { return head; }
+            set { head = value; }
+        }
+
+
+        public Lista()
+        {
+            head = null;
+        }
+
+
+        public void agregarNodo(int x)
+        {
+            Nodo nuevo = new Nodo();
+            nuevo.dato = x;
+            if (head == null)
+            {
+                nuevo.siguiente = nuevo;
+                nuevo.anterior = nuevo;
+                head = nuevo;
+                return;
+            }
+            Nodo h = head;
+            if (nuevo.dato > head.dato)
+            {
+                Nodo ultimo = head.anterior;
+                nuevo.siguiente = head;
+                nuevo.anterior = ultimo;
+                head.anterior = nuevo;
+                ultimo.siguiente = nuevo;
+                head = nuevo;
+                return;
+            }
+            while (h.dato != nuevo.dato)
+            {
+                if (nuevo.dato < h.siguiente.dato)
+                {
+                    break;
+                }
+                Nodo ultimo = head.anterior;
+                nuevo.siguiente = head;
+                nuevo.anterior = ultimo;
+                head.anterior = nuevo;
+                ultimo.siguiente = nuevo;
+                return;
+            }
+            nuevo.siguiente = h.siguiente;
+            h.siguiente = nuevo;
+
+
+            //else
+            //{
+            //    Nodo ultimo = head.anterior;
+            //    nuevo.siguiente = head;
+            //    nuevo.anterior = ultimo;
+            //    head.anterior = nuevo;
+            //    ultimo.siguiente = nuevo;
+            //}
+            Console.WriteLine("\n Nuevo nodo ingresado con exito\n\n");
+        }
+
+        public void buscarNodo()
+        {
+            Nodo actual = new Nodo();
+            actual = head;
+            bool encontrado = false;
+            Console.Write(" Ingrese el dato del nodo a buscar: ");
+            int nodoBuscado = int.Parse(Console.ReadLine());
+            if (actual != null)
+            {
+                do
+                {
+                    if (actual.dato == nodoBuscado)
+                    {
+                        Console.WriteLine("\n Nodo con el dato ({0}) Encontrado\n", actual.dato);
+                        encontrado = true;
+                    }
+                    actual = actual.siguiente;
+                } while (actual != head && encontrado != true);
+                if (!encontrado)
+                {
+                    Console.WriteLine("\n Nodo no encontrado\n");
+                }
+            }
+            else
+            {
+                Console.WriteLine("\n La lista se encuentra vacia\n");
+            }
+        }
+
+        public void modificarNodo()
+        {
+            Nodo actual = new Nodo();
+            actual = head;
+            bool encontrado = false;
+            Console.Write(" Ingrese el dato del nodo a Modificar: ");
+            int nodoBuscado = int.Parse(Console.ReadLine());
+            if (actual != null)
+            {
+                do
+                {
+                    if (actual.dato == nodoBuscado)
+                    {
+                        Console.WriteLine("\n Nodo con el dato ({0}) Encontrado\n", actual.dato);
+                        Console.Write(" Ingrese el nuevo dato para este nodo: ");
+                        actual.dato = int.Parse(Console.ReadLine());
+                        Console.WriteLine("\n Nodo modificado con exito");
+                        encontrado = true;
+                    }
+                    actual = actual.siguiente;
+                } while (actual != head && encontrado != true);
+                if (!encontrado)
+                {
+                    Console.WriteLine("\n Nodo no encontrado\n");
+                }
+            }
+            else
+            {
+                Console.WriteLine("\n La lista se encuentra vacia\n");
+            }
+        }
+
+        public void eliminarNodo()
+        {
+            Nodo actual = new Nodo();
+            actual = head;
+            Nodo anterior = new Nodo();
+            Nodo ultimo = new Nodo();
+            anterior = null;
+            ultimo = head;
+            bool encontrado = false;
+            Console.Write(" Ingrese el dato del nodo a Elminar: ");
+            int nodoBuscado = int.Parse(Console.ReadLine());
+            if (actual != null)
+            {
+                do
+                {
+                    if (actual.dato == nodoBuscado)
+                    {
+                        if (actual == head)
+                        {
+                            head = head.siguiente;
+                            head.anterior = ultimo;
+                            ultimo.siguiente = head;
+                        }
+                        else if (actual == ultimo)
+                        {
+                            ultimo = anterior;
+                            ultimo.siguiente = head;
+                            head.anterior = ultimo;
+                        }
+                        else
+                        {
+                            anterior.siguiente = actual.siguiente;
+                            actual.siguiente.anterior = anterior;
+                        }
+                        Console.WriteLine("\n Nodo eliminado con exito\n");
+                        encontrado = true;
+                    }
+                    anterior = actual;
+                    actual = actual.siguiente;
+                } while (actual != head && encontrado != true);
+                if (!encontrado)
+                {
+                    Console.WriteLine("\n Nodo no encontrado\n");
+                }
+            }
+            else
+            {
+                Console.WriteLine("\n La lista se encuentra vacia\n");
+            }
+        }
+
+        public void desplegarListaPU()
+        {
+            Nodo actual = new Nodo();
+            actual = head;
+            if (actual != null)
+            {
+                do
+                {
+                    Console.WriteLine(" " + actual.dato);
+                    actual = actual.siguiente;
+                } while (actual != head);
+            }
+            else
+            {
+                Console.WriteLine("\n La lista se encuentra vacia\n");
+            }
+        }
+
+        //public void desplegarListaUP()
+        //{
+        //    Nodo actual = new Nodo();
+        //    actual = head;
+        //    if (actual != null)
+        //    {
+        //        do
+        //        {
+        //            Console.WriteLine(" " + actual.dato);
+        //            actual = actual.anterior;
+        //        } while (actual != head);
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("\n La lista se encuentra vacia\n");
+        //    }
+        //}
+
+
+        public void Guardar(string nombreArchivo)
+        {
+            Nodo h = head;
+            if (head == null)
+            {
+                return;
+            }
+            nombreArchivo = "ListaDobleCircular";
+            string path = @"d:\" + nombreArchivo + ".txt";
+            using (StreamWriter sw = File.CreateText(path))
+            {
+                do
+                {
+                    sw.WriteLine(h.dato);
+                    h = h.siguiente;
+                } while (h != head);
+            }
+            return;
+        }
+        public void Cargar(string nombreArchivo)
+        {
+            nombreArchivo = "ListaDobleCircular";
+            string[] lineas = File.ReadAllLines(@"d:\" + nombreArchivo + ".txt");
+            foreach (string linea in lineas)
+            {
+                if (linea.Length == 0)
+                {
+                    continue;
+                }
+                string[] datos = linea.Split('-');
+                int dato = int.Parse(datos[0]);
+                Nodo x = new Nodo(dato);
+                agregarNodo(dato);
+            }
+        }
+    }
+}
---
+++ Lista Doble Circular/Lista Doble Circular/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+ 
+
+namespace Lista_Doble_Circular
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Lista l = new Lista();
+            int opcionMenu = 0;
+            do
+            {
+                Console.WriteLine("\n |-----------------------------------|");
+                Console.WriteLine("\n |      °LISTA CIRCULAR DOBLE°       |");
+                Console.WriteLine("\n |-----------------|-----------------|");
+                Console.WriteLine("\n | 1. Agregar      | 5. Desplegar P.U|");
+                Console.WriteLine("\n | 2. Buscar       | 6. Desplegar U.P|");
+                Console.WriteLine("\n | 3. Modificar    | 7. Salir        |");
+                Console.WriteLine("\n | 4. Eliminar     |                 |");
+                Console.WriteLine("\n |-----------------|-----------------|");
+                Console.Write(" Escoja una Opcion: ");
+                opcionMenu = int.Parse(Console.ReadLine());
+                switch (opcionMenu)
+                {
+                    case 1:
+                        Console.WriteLine("\n\n AGREGAR NODO EN LA LISTA \n");
+                        int numero = int.Parse(Console.ReadLine());
+                        l.agregarNodo(numero);
+                        l.Guardar("ListaDobleCircular");
+                        break;
+                       
+                    case 2:
+                        Console.WriteLine("\n\n BUSCAR UN NODO EN LA LISTA \n");
+                        l.buscarNodo();
+                        break;
+                    case 3:
+                        Console.WriteLine("\n\n MODIFICAR UN NODO EN LA LISTA \n");
+                        l.modificarNodo();
+                        break;
+                    case 4:
+                        Console.WriteLine("\n\n ELIMINAR UN NODO EN LA LISTA \n");
+                        l.eliminarNodo();
+                        break;
+                    case 5:
+                        Console.WriteLine("\n\n DESPLEGAR LISTA DE NODOS DEL PRIMERO AL ULTIMO \n");
+                        l.desplegarListaPU();
+
+                        l.Cargar("ListaDobleCircular");
+                        break;
+                    //case 6:
+                    //    Console.WriteLine("\n\n DESPEGAR LISTA DE NODOS DEL ULTIMO AL PRIMERO \n");
+                    //    l.desplegarListaUP();
+                    //    break;
+                    case 7:
+                        Console.WriteLine("\n\n Opcion no valida \n");
+                        break;
+
+
+                    default:
+                        break;
+                }
+            } while (opcionMenu !=7);
+
+
+      
+
+        }
+    }
+}
